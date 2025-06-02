c8890ea6e697faedddb37c2c385c0520c1ac607d
S3kk0um1 <s.sekkoumi8@gmail.com>
2025-06-02 10:49:12 +0200

    adding all the blockchain components

--- src/main/java/com/pm/blockchain/Main.java
+++ src/main/java/com/pm/blockchain/Main.java
@@ -2,15 +2,50 @@ package com.pm.blockchain;
 
 import com.google.gson.GsonBuilder;
 import com.pm.blockchain.blockchain.Block;
+import com.pm.blockchain.blockchain.StringUtil;
+import com.pm.blockchain.transaction.Transaction;
+import com.pm.blockchain.transaction.TransactionOutput;
+import com.pm.blockchain.wallet.Wallet;
 
+import java.security.Security;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import static com.pm.blockchain.blockchain.Chain.isChainValid;
 
 public class Main {
+
+
+
+
+
+
+
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
-    public static int difficulty = 6;
+    public static int difficulty = 5;
+    public static Wallet walletA;
+    public static Wallet walletB;
     public static void main(String[] args) {
+
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        //Create the new wallets
+        walletA = new Wallet();
+        walletB = new Wallet();
+        //Test public and private keys
+        System.out.println("Private and public keys of wallet a:");
+        System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
+        System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
+        //Create a test transaction from WalletA to walletB
+        Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
+        transaction.generateSignature(walletA.privateKey);
+        //Verify the signature works and verify it from the public key
+        System.out.println("Is signature verified");
+        System.out.println(transaction.verifySignature());
+
+
+
+
+        /*
         blockchain.add(new Block("First block of the blockchain ", "0"));
         System.out.println("Trying to Mine block 1... ");
         blockchain.get(0).mineBlock(difficulty);
@@ -28,7 +63,7 @@ public class Main {
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println("\nThe block chain: ");
         System.out.println(blockchainJson);
-
+        */
 
 
     }
---
+++ src/main/java/com/pm/blockchain/blockchain/TestChain.java
@@ -0,0 +1,16 @@
+package com.pm.blockchain.blockchain;
+
+import com.pm.blockchain.transaction.TransactionOutput;
+import com.pm.blockchain.wallet.Wallet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TestChain {
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static HashMap<String, TransactionOutput> UTXOs = new HashMap<String, TransactionOutput>(); //list of all unspent transactions.
+    public static int difficulty = 5;
+    public static Wallet walletA;
+    public static Wallet walletB;
+    public static float minimumTransaction = 0.01f;
+}
